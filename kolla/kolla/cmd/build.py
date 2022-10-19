3649a739b317f058da6e593c763f6fee757fc3b9
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-19 17:24:17 +0000

    Adding tarfile member sanitization to extractall()

--- kolla/kolla/cmd/build.py
+++ kolla/kolla/cmd/build.py
@@ -277,7 +277,26 @@ class WorkerThread(threading.Thread):
         if plugin_archives:
             for plugin_archive in plugin_archives:
                 with tarfile.open(plugin_archive, 'r') as plugin_archive_tar:
-                    plugin_archive_tar.extractall(path=plugins_path)
+                    def is_within_directory(directory, target):
+                        
+                        abs_directory = os.path.abspath(directory)
+                        abs_target = os.path.abspath(target)
+                    
+                        prefix = os.path.commonprefix([abs_directory, abs_target])
+                        
+                        return prefix == abs_directory
+                    
+                    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                    
+                        for member in tar.getmembers():
+                            member_path = os.path.join(path, member.name)
+                            if not is_within_directory(path, member_path):
+                                raise Exception("Attempted Path Traversal in Tar File")
+                    
+                        tar.extractall(path, members, numeric_owner=numeric_owner) 
+                        
+                    
+                    safe_extract(plugin_archive_tar, path=plugins_path)
         else:
             try:
                 os.mkdir(plugins_path)
